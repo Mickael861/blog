127bd1d68bde43fdef566e4340bc96d7d12dff67
Mickael861 <m-iicka86@hotmail.fr>
2022-01-19 11:01:26 +0100

    fixes #4, creation of the controller

---
+++ Src/Controller/Controller.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Controller;
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+class Controller
+{
+    
+    /**
+     * Returns the rendering of a view
+     *
+     * @param  string $view name of the view
+     * @param  array $datas the datas to be sent to the view
+     * @return string content of the view
+     */
+    public static function viewsRender(string $view, array $datas): string
+    {
+        $path_views = dirname(__DIR__, 2) . "/views";
+        $loader = new FilesystemLoader($path_views);
+
+        $twig = new Environment($loader);
+
+        $view = $view . '.twig';
+
+        return $twig->render($view, $datas);
+    }
+}
