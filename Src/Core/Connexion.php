6db7e7e601ad4faec4022808d22c5fd74fc0bd4b
Mickael861 <m-iicka86@hotmail.fr>
2022-01-20 14:33:59 +0100

    fixes#16, PDO instance recovery

---
+++ Src/Core/Connexion.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Core;
+
+use Exception;
+use PDO;
+
+class Connexion extends PDO
+{
+    private static $instance;
+
+    private const DBNAME = 'blog';
+    private const DBHOST = '127.0.0.1:3306';
+    private const DBUSER = 'root';
+    private const DBPASS = '';
+
+    public function __construct()
+    {
+        $dsn = 'mysql:dbname=' . self::DBNAME . ';host=' . self::DBHOST . '\'';
+
+        try {
+            $options = array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+            );
+            parent::__construct($dsn, self::DBUSER, self::DBPASS, $options);
+        } catch (Exception $e) {
+            exit($e->getMessage());
+        }
+    }
+    
+    /**
+     * PDO instance recovery
+     *
+     * @return PDO
+     */
+    public function getInstance(): PDO
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+}
