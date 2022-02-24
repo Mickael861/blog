4d27ee2a121a97ba4bd6aa50f34dc10d647d248d
Mickael861 <m-iicka86@hotmail.fr>
2022-02-24 15:11:01 +0100

    fixes#52, improved signupController code

--- Src/Controller/Utilisateurs/signupController.php
+++ Src/Controller/Utilisateurs/signupController.php
@@ -34,7 +34,71 @@ class SignupController extends Controller
     {
         $this->init($datas);
 
-        //signup form fields
+        $this->signupManagement();
+        
+        $this->addFormSignup();
+
+        echo $this->viewsRender($this->view, $this->datas);
+    }
+
+    /**
+     * Signup Management
+     *
+     * @return void
+     */
+    private function signupManagement()
+    {
+        $is_valide = $this->getVerifDatasForm();
+        if ($is_valide) {
+            $this->modelUtilisateurs = new UserModel;
+            $same_errors = $this->getErrorsFormSave();
+            if (!$same_errors) {
+                $this->createAccount();
+            } else {
+                $this->datas['errors'] = $same_errors;
+            }
+        }
+    }
+
+    /**
+     * Add the form to the view
+     *
+     * @return void
+     */
+    private function addFormSignup(): void
+    {
+        $this->datas['formSignup'] = $this->getFormSignup();
+    }
+
+    private function createAccount()
+    {
+        $datas = array(
+            'role' => 'utilisateur',
+            'pseudo' => $this->datas_post['pseudo'],
+            'firstname' => $this->datas_post['firstname'],
+            'lastname' => $this->datas_post['lastname'],
+            'email' => $this->datas_post['email'],
+            'password' => password_hash($this->datas_post['password'], PASSWORD_ARGON2I),
+            'statut' => 'en_attente'
+        );
+
+        $is_save = $this->modelUtilisateurs->save($datas);
+        if ($is_save) {
+            $_SESSION['success'] = 'Compte crée avec succés et en attente d\'acceptation';
+            header('Location: /');
+            exit();
+        } else {
+            $this->datas['errors'] = implode('<br>', $this->modelUtilisateurs->getErrors());
+        }
+    }
+
+     /**
+     * Verify that the form data is correct
+     *
+     * @return array|bool true the data is correct, false otherwise
+     */
+    private function getVerifDatasForm()
+    {
         $datasContactExpected = array(
             'pseudo' => 'Pseudo',
             'firstname' => 'Prénom',
@@ -43,41 +107,9 @@ class SignupController extends Controller
             "password" => 'Mot de passe'
         );
         $action = '/signup';
-        $formSignup = new Form($action, 'POST', $this->datas_post);
-        //verification form data
-        $is_valide = $formSignup->verifDatasForm($datasContactExpected);
-        if ($is_valide) {
-            $this->modelUtilisateurs = new UserModel;
-            $same_errors = $this->getErrorsFormSave();
-            if (!$same_errors) {
-                $datas = array(
-                    'role' => 'utilisateur',
-                    'pseudo' => $this->datas_post['pseudo'],
-                    'firstname' => $this->datas_post['firstname'],
-                    'lastname' => $this->datas_post['lastname'],
-                    'email' => $this->datas_post['email'],
-                    'password' => password_hash($this->datas_post['password'], PASSWORD_ARGON2I),
-                    'statut' => 'en_attente'
-                );
+        $this->formSignup = new Form($action, 'POST', $this->datas_post);
 
-                $is_save = $this->modelUtilisateurs->save($datas);
-                if ($is_save) {
-                    $_SESSION['success'] = 'Compte crée avec succés et en attente d\'acceptation';
-                    header('Location: /');
-                    exit();
-                } else {
-                    $this->datas['errors'] = implode('<br>', $this->modelUtilisateurs->getErrors());
-                }
-            } else {
-                $this->datas['errors'] = $same_errors;
-            }
-        }
-        
-        //create signup form
-        $form_signup = $this->getFormSignup($formSignup);
-        $this->datas['formSignup'] = $form_signup;
-
-        echo $this->viewsRender($this->view, $this->datas);
+        return $this->formSignup->verifDatasForm($datasContactExpected);
     }
     
     /**
@@ -100,18 +132,17 @@ class SignupController extends Controller
     /**
      * Creation of the login form
      *
-     * @param  Objet $formContact Objet of the Form
      * @return string form contact in HTML
      */
-    public function getFormSignup(Form $formSignup): string
+    public function getFormSignup(): string
     {
-        $fields = $formSignup->addInputText('pseudo', 'pseudo', 'Votre pseudo', 'text', true);
-        $fields .= $formSignup->addInputText('firstname', 'firstname', 'Votre prenom', 'text', true);
-        $fields .= $formSignup->addInputText('lastname', 'lastname', 'Votre nom', 'text', true);
-        $fields .= $formSignup->addInputText('email', 'email', 'Votre adresse e-mail', 'email', true);
-        $fields .= $formSignup->addInputText('password', 'password', 'Votre mot de passe', 'password', true);
-        $fields .= $formSignup->addButton('Créer un compte', 'margin-btn-form');
+        $fields = $this->formSignup->addInputText('pseudo', 'pseudo', 'Votre pseudo', 'text', true);
+        $fields .= $this->formSignup->addInputText('firstname', 'firstname', 'Votre prenom', 'text', true);
+        $fields .= $this->formSignup->addInputText('lastname', 'lastname', 'Votre nom', 'text', true);
+        $fields .= $this->formSignup->addInputText('email', 'email', 'Votre adresse e-mail', 'email', true);
+        $fields .= $this->formSignup->addInputText('password', 'password', 'Votre mot de passe', 'password', true);
+        $fields .= $this->formSignup->addButton('Créer un compte', 'margin-btn-form');
 
-        return $formSignup->createForm($fields);
+        return $this->formSignup->createForm($fields);
     }
 }
